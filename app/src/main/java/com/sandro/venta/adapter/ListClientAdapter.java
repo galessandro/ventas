2256d10679d4cfb94fc7bae5f0c117a10cc49dba
German Granados <ggranados@sunat.gob.pe>
2019-03-25 16:27:48 -0500

    feat: Add RecyclerView of clients

---
+++ app/src/main/java/com/sandro/venta/adapter/ListClientAdapter.java
@@ -0,0 +1,60 @@
+package com.sandro.venta.adapter;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.sandro.venta.R;
+import com.sandro.venta.bean.ListClient;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ListClientAdapter extends RecyclerView.Adapter<ListClientAdapter.ViewHolder> {
+
+    List<ListClient> lstClients;
+    Context ctx;
+
+    public ListClientAdapter(List<ListClient> lstClients, Context ctx) {
+        this.lstClients = lstClients;
+        this.ctx = ctx;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.list_client_item_rcy, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int i) {
+        ListClient client = lstClients.get(i);
+        holder.tvDni.setText(client.getDni());
+        holder.tvName.setText(client.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstClients.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.txtItemClientRuc) TextView tvDni;
+        @BindView(R.id.txtItemClientBussinnesName) TextView tvName;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
--- app/src/main/java/com/sandro/venta/fragment/TabClients.java
+++ app/src/main/java/com/sandro/venta/fragment/TabClients.java
@@ -2,19 +2,54 @@ package com.sandro.venta.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.sandro.venta.R;
+import com.sandro.venta.adapter.ListClientAdapter;
+import com.sandro.venta.bean.ListClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class TabClients extends Fragment {
+
+    @BindView(R.id.rcyViewClients)
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+
+    Unbinder unbinder;
+
+    private List<ListClient> lstClients;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tab_clients, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        lstClients = new ArrayList<>();
+
+        for (int i = 0; i <= 20; i++){
+            ListClient client = new ListClient(
+                    "dni " + i,
+                    "name " + i
+            );
+            lstClients.add(client);
+        }
+        adapter = new ListClientAdapter(lstClients, this.getContext());
+        recyclerView.setAdapter(adapter);
+
         return rootView;
     }
 }
